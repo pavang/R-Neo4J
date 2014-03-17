90ae9b7836d48de2750b9eec13796e7af71eb0d0
pavang <kranthi.gandrapu@gmail.com>
2014-03-17 08:13:49 -0500

    Update first.R

--- R/first.R
+++ R/first.R
@@ -1,22 +1,15 @@
 library("RCurl")
 library("RJSONIO")
 
-graphDB <- setRefClass("graphDB",
-                       fields=list(uri="character",creds="character",graphRoot="list"),
-                       methods=list(
-                          getGraphRoot=function(){
-                            graphRoot
-                         }))
-
-connectGraph <- function(uri,userName,password) {
-  h <- basicTextGatherer()
-  headers <- list('Accept' = 'application/json;charset=UTF-8', 'Content-Type' = 'application/json')
-  creds<-paste(userName,password,sep=":")
-  curlPerform(url=uri,.opts=list(userpwd=creds, httpheader=headers),writefunction = h$update)
-  root<-fromJSON(h$value())
-  graphDB$new(uri=uri,creds=creds,graphRoot=root)
+#Utility to connect to the graph DB
+connectGraphDb<-function(dburl,port,usr,pwd){
+  auth<-paste(usr,":",pwd,sep='')
+  baseurl<-paste(dburl,":",port,"/db/data/",sep="")
+  db<-getURL(paste(baseurl),userpwd=paste(auth))
+  return(list(db=fromJSON(db),auth=auth,baseurl=baseurl))
 }
 
+#Function to perform graph traversal given the start node
 graphTraverse <- function(db,startNode=0,returnType="path",order="breadth_first",uniqueness="none",language="builtin",retPropName="property",retPropVal="all") {
   h <- basicTextGatherer()
   headers <- list('Accept' = 'application/json;charset=UTF-8', 'Content-Type' = 'application/json')
@@ -24,22 +17,50 @@ graphTraverse <- function(db,startNode=0,returnType="path",order="breadth_first"
   names(returnVal)[2]<-eval(retPropName)
   param<-list(order=order,uniqueness=uniqueness,return_filter=returnVal)
   uriAppend<-paste("node",startNode,"traverse",returnType,sep="/")
-  uri<-paste(db$uri,uriAppend,sep="")
-  curlPerform(url=uri,.opts=list(userpwd=db$creds,postfields=toJSON(param), httpheader=headers),writefunction = h$update)
+  uri<-paste(db$baseurl,uriAppend,sep="")
+  curlPerform(url=uri,.opts=list(userpwd=db$auth,postfields=toJSON(param), httpheader=headers),writefunction = h$update)
   fromJSON(h$value())
 }
 
-executeCypher <- function(db,startNode,returnProp="property"){
+#Function to do simple query given the node
+executeCypherStartQuery <- function(db,startNode,returnProp="property"){
   h <- basicTextGatherer()
   headers <- list('Accept' = 'application/json;charset=UTF-8', 'Content-Type' = 'application/json')
   executeQuery<-list(query=paste("start x = node(",startNode,") return x.",eval(returnProp),sep=""),params={})
-  print(toJSON(executeQuery))
-  curlPerform(url=db$graphRoot$cypher,.opts=list(userpwd=db$creds,postfields=toJSON(executeQuery), httpheader=headers),writefunction = h$update)
+  curlPerform(url=db$db$cypher,.opts=list(userpwd=db$auth,postfields=toJSON(executeQuery), httpheader=headers),writefunction = h$update)
   fromJSON(h$value())
 }
 
-db<-connectGraph("http://test.sb01.stations.graphenedb.com:24789/db/data/","Test","WZKpp22qJnYd9LGBFNPN")
+#Function to retrieve all the nodes matching a name
+executeCypherMatchQuery <- function(db,nodeName){
+  h <- basicTextGatherer()
+  headers <- list('Accept' = 'application/json;charset=UTF-8', 'Content-Type' = 'application/json')
+  executeQuery<-list(query=paste("match (x:`",nodeName,"`) return x",sep=""),params={})
+  curlPerform(url=db$db$cypher,.opts=list(userpwd=db$auth,postfields=toJSON(executeQuery), httpheader=headers),writefunction = h$update)
+  fromJSON(h$value())
+}
+
+#Function to retrieve all the nodes matching a name and with the provided value
+executeCypherMatchWhereQuery <- function(db,nodeName,whereCond,whereVal){
+  h <- basicTextGatherer()
+  headers <- list('Accept' = 'application/json;charset=UTF-8', 'Content-Type' = 'application/json')
+  executeQuery<-list(query=paste("match (x:`",nodeName,"`) where x.",whereCond,"=", whereVal,"return x",sep=""),params={})
+  curlPerform(url=db$db$cypher,.opts=list(userpwd=db$auth,postfields=toJSON(executeQuery), httpheader=headers),writefunction = h$update)
+  fromJSON(h$value())
+}
+
+#Function to execute CypherQuery given the JSON input
+executeCypherQueryJSON <- function(db,json=""){
+  h <- basicTextGatherer()
+  headers <- list('Accept' = 'application/json;charset=UTF-8', 'Content-Type' = 'application/json')
+  curlPerform(url=db$db$cypher,.opts=list(userpwd=db$auth,postfields=json, httpheader=headers),writefunction = h$update)
+  fromJSON(h$value())
+}
+
+db<-connectGraphDb("http://finalprojtest.sb01.stations.graphenedb.com",24789,"finalproj_test","VD8XYRgY8SwO4RdCEKqc")
 traverse<-graphTraverse(db,startNode=52,retPropName="name")
-executeCypher(db=db,startNode=15,returnProp="tagline")
+executeCypherStartQuery(db=db,startNode=15,returnProp="tagline")
+executeCypherMatchQuery(db=db,nodeName="Person")
+executeCypherMatchWhereQuery(db=db,nodeName="Person",whereCond="born",whereVal=1971)
 
 
